4a3971216db5e51f304ab018f8635258d5e63991
Antonin Ginet <agintonin98@gmail.com>
2023-06-13 14:16:41 +0000

    Use BASE_URL to export prefixBase

--- src/utils/global.ts
+++ src/utils/global.ts
@@ -1,4 +1,4 @@
-export const prefixBase = '/portfolio';
+export const prefixBase = import.meta.env.BASE_URL === '/' ? '/' : import.meta.env.BASE_URL.slice(0, -1);
 
 export function isObjKey<T extends Object>(key: PropertyKey, obj: T): key is keyof T {
   return key in obj;
