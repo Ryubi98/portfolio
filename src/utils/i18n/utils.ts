7ef6986290710abf45e1425df353fdc8d3fad729
Antonin Ginet <agintonin98@gmail.com>
2023-06-21 15:16:01 +0000

    Refacto translations in class handler to correctly test it in the future

--- src/utils/i18n/utils.ts
+++ src/utils/i18n/utils.ts
@@ -1,14 +1,17 @@
-import { translations } from '@utils/i18n/translations';
-import { isObjKey, prefixBase } from '@utils/global';
-
-export const defaultLang = 'fr';
-export const slugLang: Lang[] = ['fr', 'en'];
+import translations from '@utils/i18n/translations.json';
+import TranslationsHandler from '@utils/i18n/TranslationsHandler';
+import { prefixBase } from '@utils/global';
 
 export type Lang = keyof typeof translations;
-export type LangKey = keyof (typeof translations)[typeof defaultLang];
 export type LangParams = {
   lang: Lang;
 };
+
+export const defaultLang: Lang = 'fr';
+export const slugLang: Lang[] = ['fr', 'en'];
+
+const translationsHandler = new TranslationsHandler(translations, defaultLang);
+export const useTranslations = translationsHandler.useTranslations;
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.replace(prefixBase, '').split('/');
@@ -20,36 +23,6 @@ export function getUrlFromDestinationAndLang(destination: string, lang: Lang = d
   const destinationWithNoStartingSlash = destination.replace(/^\//, '');
   const paths = [prefixBase, lang, ...(destination ? [destinationWithNoStartingSlash] : [])];
   return paths.join('/');
-}
-
-export function useTranslations(lang: Lang) {
-  return function t(key: keyof (typeof translations)[typeof defaultLang], replace: { [key: string]: Object } = {}) {
-    const result: Object[] = isObjKey(key, translations[lang])
-      ? [translations[lang][key]]
-      : [translations[defaultLang][key]];
-
-    Object.entries(replace).forEach(([replaceKey, replaceValue]: [string, Object]) => {
-      result.forEach((value: Object, index: number) => {
-        // even values in result are string litterals
-        // odd values in result are injected from replace object parameter
-        // only iterate on even values, we do not want to touch replaced values
-        if (index % 2 !== 0) return;
-
-        const arrayWithoutReplaceKey = value.toString().split(`{${replaceKey}}`);
-        const arrayWithReplaceValue = arrayWithoutReplaceKey.reduce(
-          (accumulator: Object[], value: string, index: number) => [
-            ...accumulator,
-            value,
-            ...(arrayWithoutReplaceKey.length - 1 === index ? [] : [replaceValue]),
-          ],
-          []
-        );
-        result.splice(index, 1, ...arrayWithReplaceValue);
-      });
-    });
-
-    return result;
-  };
 }
 
 export async function getLangStaticPaths() {
