892684fb292bc3406f1af9f72da584e4c3ad74c8
Antonin Ginet <agintonin98@gmail.com>
2024-02-09 10:46:42 +0100

    Refacto utils

--- src/utils/i18n/utils.ts
+++ src/utils/i18n/utils.ts
@@ -1,40 +1,50 @@
-import { prefixBase } from '@utils/global';
-import TranslationsHandler from '@utils/i18n/TranslationsHandler';
-import translations from '@utils/i18n/translations.json';
+import { isObjKey } from '@utils/global';
+import translations from './translations.json';
 
 export type Lang = keyof typeof translations;
 export type LangParams = {
   lang: Lang;
 };
-export type TranslatedMessage = Object[];
 
-export const defaultLang: Lang = 'fr';
-export const slugLangs: Lang[] = ['fr', 'en'];
-
-const translationsHandler = new TranslationsHandler(translations, defaultLang);
-export const useTranslations = translationsHandler.useTranslations;
-
-export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.replace(prefixBase, '').split('/');
-  if (lang in translations) return lang as Lang;
-  return defaultLang;
-}
-
-export function getDestinationFromUrl(url: URL) {
-  const [, , ...destination] = url.pathname.replace(prefixBase, '').split('/');
-  return `/${destination.join('/')}`;
-}
-
-export function getUrlFromDestinationAndLang(destination: string, lang: Lang = defaultLang) {
-  const destinationWithNoStartingSlash = destination.replace(/^\//, '');
-  if (destinationWithNoStartingSlash.length === 0 || destinationWithNoStartingSlash.startsWith('#')) {
-    return `${prefixBase}/${lang}${destinationWithNoStartingSlash}`;
-  }
-  return `${prefixBase}/${lang}/${destinationWithNoStartingSlash}`;
-}
+export const defaultLang = 'fr';
+export const slugLangs = Object.keys(translations) as Lang[];
 
 export async function getLangStaticPaths() {
   return slugLangs.map((lang) => ({
     params: { lang },
   }));
+}
+export type TranslatedMessagePart = string | object;
+export type TranslatedMessage = TranslatedMessagePart[];
+
+export function useTranslations(lang: Lang) {
+  return function t(
+    key: keyof (typeof translations)[typeof defaultLang],
+    replace: Record<string, TranslatedMessagePart> = {}
+  ) {
+    const result: TranslatedMessage = [
+      isObjKey(key, translations[lang]) ? translations[lang][key] : translations[defaultLang][key],
+    ];
+
+    Object.entries(replace).forEach(([replaceKey, replaceValue]: [string, TranslatedMessagePart]) => {
+      result.forEach((value: TranslatedMessagePart, index: number) => {
+        // even values in result are string literals
+        // odd values in result are injected from replace object parameter
+        // only iterate on even values (should always be a string), we do not want to touch replaced values
+        if (index % 2 === 1 || typeof value !== 'string') return;
+
+        const arrayWithoutReplaceKey = value.split(`{${replaceKey}}`);
+        const arrayWithReplaceValue = arrayWithoutReplaceKey.reduce(
+          (accumulator: TranslatedMessage, value: TranslatedMessagePart, index: number) => [
+            ...accumulator,
+            value,
+            ...(index < arrayWithoutReplaceKey.length - 1 ? [replaceValue] : []),
+          ],
+          []
+        );
+        result.splice(index, 1, ...arrayWithReplaceValue);
+      });
+    });
+    return result;
+  };
 }
