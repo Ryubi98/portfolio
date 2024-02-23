0ce9a2f4d83942aa6c7753b7868bacf3912f9ced
Antonin Ginet <agintonin98@gmail.com>
2024-02-23 16:40:13 +0100

    Update astro dep and install astro-icon

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,14 @@
 import mdx from '@astrojs/mdx';
 import pluginBasicSsl from '@vitejs/plugin-basic-ssl';
 import astroCompress from 'astro-compress';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ryubi98.github.io',
   base: '/portfolio',
-  integrations: [astroCompress(), mdx()],
+  integrations: [astroCompress(), mdx(), icon()],
   vite: {
     plugins: [pluginBasicSsl()],
   },
